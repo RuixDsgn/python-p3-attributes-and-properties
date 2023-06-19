482c5b3f15359381134657d1b3294f0953429f36
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-18 22:14:00 -0700

    complete lab with working code but tests do not pass

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
+class Dog:
+
+    APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -9,7 +11,34 @@ APPROVED_BREEDS = [
     "French Bulldog",
     "Pug",
     "Pointer"
-]
+    ]
 
-class Dog:
-    pass
+    def __init__(self):
+        self._name = None
+        self._breed = None
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) == str) and 1 <= len(name) <= 25:
+            print(f"Setting name to {name}")
+            self._name = name
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in self.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
+    
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+
+    
+        
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,29 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+
+    def __init__(self):
+        self._name = None
+        self._job = None
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            print(f"Setting name to {name}")
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in Person.approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
